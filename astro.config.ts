f71273cce2bdaa1d40cc613074ebf578edb198d8
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2023-01-13 01:03:15 +0900

    feat: support exact name match

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,25 @@ export default defineConfig({
 
       return remarkObsidian({
         titleToUrl: (title) => {
-          const index = mdFileList.findIndex(mdPath => mdPath.includes(title));
+          const filteredFiles = mdFileList
+            .map((mdPath, i) => {
+              const splited = mdPath.split('/');
+              const filename = splited[splited.length - 1];
+              return (filename.includes(title)) ? i : -1;
+            }).filter((e) => e !== -1);
+
+          let index = -1;
+          if (filteredFiles.length > 1) {
+            const filenames = filteredFiles.map((fIndex, i) => ({ filename: mdFileList[fIndex], index: fIndex }))
+            filenames.forEach((fn) => {
+              const filename = fn.filename.split('/');
+              if (filename === title) index === fn.index; // Exact match
+              else index = fn.index; // Partial match
+            })
+          } else {
+            index = (filteredFiles.length > 0) ? filteredFiles[0] : -1;
+          }
+
           if (index !== -1) {
             const { dir, filename } = helperDirsAndFile(mdFileList[index]);
             return `/garden/${generatePathSlug(dir, filename)}`;
